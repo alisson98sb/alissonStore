33e825ea0bf2f58ca5af28b2306393637ace7f93
alisson98sb <alisson98sb@gmail.com>
2021-09-25 15:56:02 -0300

    aula 02 finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HighlightsComponent } from './home/highlights/highlights.component';
+import { CategoriesComponent } from './home/highlights/categories/categories.component';
+import { PopularComponent } from './home/popular/popular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HighlightsComponent,
+    CategoriesComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule
